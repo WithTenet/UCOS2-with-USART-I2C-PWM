9b3f881a9cc47a7b54288f2b02b35c9b05786249
Jay_Mngr <54743766+JasonIlIlIlIl@users.noreply.github.com>
2020-09-07 14:27:27 +0800

    commit again

---
+++ BSP/HSE/HSE_init.c
@@ -0,0 +1,1 @@
+#include "HSE_init.h"
---
+++ BSP/HSE/HSE_init.h
@@ -0,0 +1,8 @@
+/********* Method for use HSE as system clock base
+ * Jason 2020.9.7
+ * *******************/
+
+
+#include "stm32f4xx.h"
+#include <stdio.h>
+
