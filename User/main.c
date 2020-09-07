136a9ae9fdfe84ccfb3b3f57cc95eb339310984b
Jay_Mngr <54743766+JasonIlIlIlIl@users.noreply.github.com>
2020-09-07 12:59:16 +0800

    "newtest for altern"

--- User/main.c
+++ User/main.c
@@ -7,12 +7,13 @@ OS_STK		App_Task2Stack[APP_TASK2_STK_SIZE];
 int ticks=0;
 int main(void)
 { 
-	
+	int newtest=0;
 	BSP_Init();
 	OSInit();   
 	OSTaskCreate(App_Task0,(void *)0,(OS_STK *)&App_Task0Stack[APP_TASK0_STK_SIZE-1],APP_TASK0_PRIO );
 	//OSTaskCreate(App_Task1,(void *)0,(OS_STK *)&App_Task1Stack[APP_TASK1_STK_SIZE-1],APP_TASK1_PRIO );
 	//OSTaskCreate(App_Task2,(void *)0,(OS_STK *)&App_Task2Stack[APP_TASK2_STK_SIZE-1],APP_TASK2_PRIO );
+	
 	OSStart();	
 }
 
